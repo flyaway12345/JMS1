159955d5990eb437d76f43c421ec7beccc241de3
Evan Haggard <evanhaggard123@gmail.com>
2020-06-03 10:30:03 -0400

    first commit

---
+++ src/main/java/him/haggardinnovations/sfjms/SfJmsApplication.java
@@ -0,0 +1,13 @@
+package him.haggardinnovations.sfjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/him/haggardinnovations/sfjms/SfJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package him.haggardinnovations.sfjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
